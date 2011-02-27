be27d453515a14929556e0aa47d8664289a5d538
Tomas Celizna <tomas.celizna@gmail.com>
2011-02-27 21:57:23 +0100

    add MongoMapper::Datatypes namespace

--- lib/mongo_mapper/datatypes/permalink.rb
+++ lib/mongo_mapper/datatypes/permalink.rb
@@ -3,17 +3,23 @@
 
 # this class implements permalink data type
 
-class Permalink < String
+module MongoMapper
+  module Datatypes
+
+    class Permalink < String
 	
-	def self.to_mongo(value)
-		ActiveSupport::Inflector.transliterate(value).		# remove accents
-		gsub(/[^a-zA-Z0-9_\s]/, '').											# remove non-alphanumerical characters (except _)
-		gsub(/\b\w/){$&.upcase}.													# titlecase
-		gsub(/\s+/, '')	unless value.nil?									# remove space
-	end
+    	def self.to_mongo(value)
+    		ActiveSupport::Inflector.transliterate(value).		# remove accents
+    		gsub(/[^a-zA-Z0-9_\s]/, '').											# remove non-alphanumerical characters (except _)
+    		gsub(/\b\w/){$&.upcase}.													# titlecase
+    		gsub(/\s+/, '')	unless value.nil?									# remove space
+    	end
 	
-	def self.from_mongo(value)
-		value
-	end
+    	def self.from_mongo(value)
+    		value
+    	end
 	
+    end
+
+  end
 end
--- test/permalink_datatype_test.rb
+++ test/permalink_datatype_test.rb
@@ -5,37 +5,37 @@ class PermalinkDatatypeTest < ActiveSupport::TestCase
 	context "Permalink.to_mongo" do
 		
 		should "remove whitespace" do
-			assert_equal Permalink.to_mongo("Some Page Name"), "SomePageName"
-			assert_equal Permalink.to_mongo("     Some        Page      Name   "), "SomePageName"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("Some Page Name"), "SomePageName"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("     Some        Page      Name   "), "SomePageName"
 		end
 		
 		should "keep underscores" do
-		  assert_equal Permalink.to_mongo("Some_Page_Name"), "Some_Page_Name"
-		  assert_equal Permalink.to_mongo("Some_page_name"), "Some_page_name"
+		  assert_equal MongoMapper::Datatypes::Permalink.to_mongo("Some_Page_Name"), "Some_Page_Name"
+		  assert_equal MongoMapper::Datatypes::Permalink.to_mongo("Some_page_name"), "Some_page_name"
 	  end
 
 		should "capitalize string" do
-			assert_equal Permalink.to_mongo("poo"), "Poo"
-			assert_equal Permalink.to_mongo("Some page name"), "SomePageName"
-			assert_equal Permalink.to_mongo("Some pAGE name"), "SomePAGEName"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("poo"), "Poo"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("Some page name"), "SomePageName"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("Some pAGE name"), "SomePAGEName"
 		end
 
 		should "remove non alpha-numerical characters" do
-			assert_equal Permalink.to_mongo("Some? page! n@#(a)&^me"), "SomePageName"
-			assert_equal Permalink.to_mongo("Small-Paul is Here"), "SmallPaulIsHere"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("Some? page! n@#(a)&^me"), "SomePageName"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("Small-Paul is Here"), "SmallPaulIsHere"
 		end
 
 		should "remove accents" do
-			assert_equal Permalink.to_mongo("Šarí půlenec Močil"), "SariPulenecMocil"
-			assert_equal Permalink.to_mongo("MĚLČINA"), "MELCINA"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("Šarí půlenec Močil"), "SariPulenecMocil"
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo("MĚLČINA"), "MELCINA"
 		end
 		
 		should "work for nil" do
-			assert_nil Permalink.to_mongo(nil)
+			assert_nil MongoMapper::Datatypes::Permalink.to_mongo(nil)
 		end
 
 		should "works for space" do
-			assert_equal Permalink.to_mongo(" "), ""
+			assert_equal MongoMapper::Datatypes::Permalink.to_mongo(" "), ""
 		end
 		
 	end
